13f7beba3a713366655da27449a80a362f05add4
florent.haffner <florent.haffner@protonmail.com>
2020-04-23 19:02:07 +0200

    added TypeScript to V1.5

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-typescript`,
     `gatsby-plugin-offline`,
     {
       resolve: `gatsby-source-filesystem`,
