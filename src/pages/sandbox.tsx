8461a5c4dd72859c87a812c040458f83f7b71ace
florent.haffner <florent.haffner@protonmail.com>
2020-05-21 18:33:17 +0200

    I'm bored by CSS, it's optimised but CRAP I hate it !

--- src/pages/sandbox.tsx
+++ src/pages/sandbox.tsx
@@ -3,11 +3,30 @@ import '../style/main.scss';
 import StatusBar from '../components/StatusBar/statusBar';
 import Chat from '../components/Chat/Chat';
 
+const Card = data => (
+  <div className="project-card">
+    <p className="card-title">{data.text}</p>
+    <p className="card-content">{data.content}</p>
+  </div>
+);
+
 export default () => {
   return (
     <>
       <StatusBar />
-      <div className="sandbox-base">Welcome from sandbox</div>
+      <div className="sandbox-base">
+        <p>Welcome from sandbox</p>
+        <div className="container-card">
+          <Card
+            text="Chatbot"
+            content="I didn't wanted to have a contact form so I build a chatbot."
+          />
+          <Card
+            text="Incognito"
+            content="A chat to play around with real-time data !"
+          />
+        </div>
+      </div>
       <Chat />
     </>
   );
